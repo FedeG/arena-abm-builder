45d878acdf6d69c7ed3b6a7758a4a1f7ebe4994f
Federico Gonzalez <federicog@exo.com.ar>
2014-09-23 17:17:20 -0300

    Mergeando ...

---
+++ src/main/java/annotations/abm/AddMethod.java
@@ -0,0 +1,12 @@
+package annotations.abm;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface AddMethod {
+
+}
