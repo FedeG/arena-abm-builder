71cafc4afcb4777893b5d47d67f9e05a16a83fa9
Charlyzzz <erwincdl@gmail.com>
2014-09-23 17:19:44 -0300

    PrimitiveAnnotation

--- src/main/java/annotations/visualWidgets/PrimitiveAnnotation.java
+++ src/main/java/annotations/visualWidgets/PrimitiveAnnotation.java
@@ -9,4 +9,14 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 public @interface PrimitiveAnnotation {
 
+	boolean validator() default true; // Si es valido o no mediante un validador
+
+	boolean required() default true; // Si el campo es requerido si o si
+
+	boolean modifiable() default true; // Si el campo se puede modificar
+
+	boolean inTable() default true; // Si se muestra en la tabla de datos
+
+	boolean canFilter() default true; // Si se puede filtrar por este campo
+
 }
