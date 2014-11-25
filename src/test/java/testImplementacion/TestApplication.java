f3c93bab74827fb7c2a23dca39f6779cedc7cc20
fedeG <federicogonzalez761@gmail.com>
2014-11-25 02:12:03 -0300

    Nuevo Test de la aplicacion para Auto y Chofer

--- src/test/java/testImplementacion/TestApplication.java
+++ src/test/java/testImplementacion/TestApplication.java
@@ -1,18 +1,22 @@
 package testImplementacion;
 
-import implementation.applicationModel.ABMApplicationModel;
-import implementation.ui.MainWindow;
+import java.util.ArrayList;
+
+import implementation.FWObject;
+import implementation.applicationModel.MultiABMApplicationModel;
+import implementation.ui.MultiMainWindow;
 
 import org.uqbar.arena.Application;
 import org.uqbar.arena.windows.Window;
 
 public class TestApplication extends Application {
-
+	
 	@Override
 	protected Window<?> createMainWindow() {
-
-		return new MainWindow(this, new ABMApplicationModel(TestObject.class));
-
+		ArrayList<Class<? extends FWObject>> lista = new ArrayList<Class<? extends FWObject>>();
+		lista.add(Auto.class);
+		lista.add(Chofer.class);
+		return new MultiMainWindow(this, new MultiABMApplicationModel(lista));
 	}
 
 	public static void main(String[] args) {
