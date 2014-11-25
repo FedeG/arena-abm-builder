637d9dfcd344dce3fecf06e1db7f59beef9d5b1d
fedeG <federicogonzalez761@gmail.com>
2014-11-25 02:10:23 -0300

    Mejora para Auto (objeto de prueba)

--- src/test/java/testImplementacion/Auto.java
+++ src/test/java/testImplementacion/Auto.java
@@ -7,31 +7,27 @@ import annotations.visualWidgets.FieldSelector;
 import annotations.visualWidgets.FieldText;
 
 @Title(title = "Autos ABM")
-public class TestObject extends FWObject {
+public class Auto extends FWObject {
 
-	@FieldText(modifiable = true,name="Nombre del automotor")
+	@FieldText(modifiable = true, name = "Nombre del automotor")
 	private String nombre;
-	
+
 	@FieldCheck(modifiable = false, name = "Tiene patente")
 	public boolean tienePatente;
-	
-	@FieldSelector(choices="provincias",modifiable = false, name = "Provincias")
+
+	@FieldSelector(choices = "provincias", modifiable = false, name = "Provincias")
 	public String provincia;
-	String[] provincias= {"Buenos Aires","Cordoba","La Rioja","Chaco","Catamarca"};
-	
-	
-	
-	
-	
-	
-	public  String[] getProvincias(){
+	String[] provincias = { "Buenos Aires", "Cordoba", "La Rioja", "Chaco",
+			"Catamarca" };
+
+	public String[] getProvincias() {
 		return provincias;
 	}
-	
-	public  void setProvincias(){
-		
+
+	public void setProvincias() {
+
 	}
-	
+
 	public boolean getTienePatente() {
 		return tienePatente;
 	}
