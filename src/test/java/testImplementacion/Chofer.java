8bc85f36b541f5d240904fd80ac56bd4e6d9ddc0
fedeG <federicogonzalez761@gmail.com>
2014-11-25 21:39:22 -0300

    Uso y ejemplo para la nueva annotation para el uso de metodos de add, remove and edit

--- src/test/java/testImplementacion/Chofer.java
+++ src/test/java/testImplementacion/Chofer.java
@@ -1,6 +1,9 @@
 package testImplementacion;
 
-import java.lang.reflect.Method;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.regex.Pattern;
 
 import implementation.FWObject;
@@ -9,7 +12,8 @@ import annotations.visualWidgets.FieldCheck;
 import annotations.visualWidgets.FieldSelector;
 import annotations.visualWidgets.FieldText;
 
-@Title(title = "Chofer ABM")
+@SuppressWarnings("serial")
+@Title(title = "Chofer ABM", addMethod="add", editMethod="edit", removeMethod="remove")
 public class Chofer extends FWObject {
 
 	@FieldText(name = "Nombre del chofer", validator="validateName")
@@ -71,4 +75,39 @@ public class Chofer extends FWObject {
 		return Pattern.compile(NAME_PATTERN).matcher(name).matches();
 	}
 
+	@Override
+	public String toString() {
+		String texto = this.nombre + ", con domicilio en " + this.domicilio + " (provincia: " + this.provincia + ") y ";
+		if (this.tieneAuto) return texto + "tiene auto";
+		else return texto + " no tiene auto";
+	}
+
+	public void add(Chofer chofer){
+		this.loggeo("Se agrego -> " + chofer.toString());
+	}
+
+	public void edit(Chofer chofer){
+		this.loggeo("Se modifico -> " + chofer.toString());
+	}
+	
+	public void remove(Chofer chofer){
+		this.loggeo("Se elimino -> " + chofer.toString());
+	}
+
+	public void loggeo(String texto){
+		 Logger logger = Logger.getLogger("ChoferLog");  
+		 FileHandler fh;  
+
+		try {
+		fh = new FileHandler("/tmp/choferes.log");
+			logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+			logger.info(texto);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
